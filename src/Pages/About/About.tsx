bda6e1607c4ab80ee2f1f26ceb54d8a2c969a35d
Tan Phat <nguyenvantanphat.it@gmail.com>
2023-05-25 11:19:09 +0700

    Commit Land

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
-// import Profile_1 from '../../img/123.svg';
-import facebook from '../../img/facebook.jpg';
-import ins from '../../img/ins.jpg';
-import git from '../../img/git.png';
-import gmail from '../../img/gmail.jpg';
+import Profile_1 from '../../Img/123.svg';
+import facebook from '../../Img/facebook.jpg';
+import ins from '../../Img/ins.jpg';
+import git from '../../Img/git.png';
+import gmail from '../../Img/gmail.jpg';
 import './About.css';
 import { ArrowRightOutlined } from '@ant-design/icons';
 
@@ -14,26 +14,26 @@ const About: React.FC = () => {
         <div
             className="header-container"
             style={{
-                // backgroundImage: `url(${Profile_1})`,
-                // backgroundRepeat: 'no-repeat',
-                // backgroundSize: 'cover',
-                // borderRadius: '30px'
+                backgroundImage: `url(${Profile_1})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                borderRadius: '30px'
             }}
         >
             <Row
             >
                 <Col className="footer-social-icons">
                     <a href="https://www.facebook.com/tanphat05062001" target="_blank">
-                        <img src={facebook} alt="Facebook" className='img' />
+                        <img src={facebook} alt="Facebook" className='img-about' />
                     </a>
                     <a href="https://www.instagram.com/tannnphatttttttt/" target="_blank">
-                        <img src={ins} alt="Instagram" className='img' />
+                        <img src={ins} alt="Instagram" className='img-about' />
                     </a>
                     <a href="https://github.com/nguyenvantanphatit" target="_blank">
-                        <img src={git} alt="Github" className='img' />
+                        <img src={git} alt="Github" className='img-about' />
                     </a>
                     <a href="https://www.gmail.com/nguyenvantanphat.it@gmail.com" target="_blank">
-                        <img src={gmail} alt="Gmail" className='img' />
+                        <img src={gmail} alt="Gmail" className='img-about' />
                     </a>
                     <Link to="/">
                         <ArrowRightOutlined style={{ fontSize: 50, color: '#000', marginLeft: 30 }} />
